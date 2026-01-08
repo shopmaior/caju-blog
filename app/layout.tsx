87bc8fcfd884ac80195a5abe795464ef433965ce
Ruan Aragao <ruanaragao2@gmail.com>
2026-01-07 23:15:40 -0300

    feat(pwa): add Progressive Web App support
    - Add `manifest.json` for PWA capabilities
    - Include various Android and Apple icons
    - Update favicon and PWA icon paths
    - Enhance app installability and user experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,28 +60,28 @@ export const metadata: Metadata = {
   // Icons
   icons: {
     icon: [
-      { url: "/icons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/icons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/icons/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
     ],
     apple: [
-      { url: "/icons/apple-icon-57x57.png", sizes: "57x57" },
-      { url: "/icons/apple-icon-60x60.png", sizes: "60x60" },
-      { url: "/icons/apple-icon-72x72.png", sizes: "72x72" },
-      { url: "/icons/apple-icon-76x76.png", sizes: "76x76" },
-      { url: "/icons/apple-icon-114x114.png", sizes: "114x114" },
-      { url: "/icons/apple-icon-120x120.png", sizes: "120x120" },
-      { url: "/icons/apple-icon-144x144.png", sizes: "144x144" },
-      { url: "/icons/apple-icon-152x152.png", sizes: "152x152" },
-      { url: "/icons/apple-icon-180x180.png", sizes: "180x180" },
+      { url: "/apple-icon-57x57.png", sizes: "57x57" },
+      { url: "/apple-icon-60x60.png", sizes: "60x60" },
+      { url: "/apple-icon-72x72.png", sizes: "72x72" },
+      { url: "/apple-icon-76x76.png", sizes: "76x76" },
+      { url: "/apple-icon-114x114.png", sizes: "114x114" },
+      { url: "/apple-icon-120x120.png", sizes: "120x120" },
+      { url: "/apple-icon-144x144.png", sizes: "144x144" },
+      { url: "/apple-icon-152x152.png", sizes: "152x152" },
+      { url: "/apple-icon-180x180.png", sizes: "180x180" },
     ],
     other: [
-      { rel: "icon", url: "/icons/android-icon-192x192.png", sizes: "192x192" },
+      { rel: "icon", url: "/android-icon-192x192.png", sizes: "192x192" },
     ],
   },
 
   // Manifest
-  manifest: "/icons/manifest.json",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
